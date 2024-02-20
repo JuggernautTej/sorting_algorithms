5984a11797f082a8f1f008112b9d431e242de13c
Benjamin <nyllary@gmail.com>
2024-02-19 20:05:40 -0800

    Readme, sort.h, print_list, print_array have been created and pushing

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -5,12 +5,12 @@
  * @n: The first variable
  * @m: The second variable
  * Return: void.
-*/
+ */
 void swap(int *n, int *m)
 {
-int temp = *n;
-*n = *m;
-*m = temp;
+	int temp = *n;
+	*n = *m;
+	*m = temp;
 }
 
 /**
@@ -18,25 +18,26 @@ int temp = *n;
  * @array: an array of integers
  * @size: the size of the array
  * Return: void.
-*/
+ */
 
 void bubble_sort(int *array, size_t size)
 {
-size_t x, y;
-int swapped;
-for (x = 0; x < size - 1; x++)
-{
-swapped = 0;
-for (y = 0; y < size - x - 1; y++)
-{
-if (array[y] > array[y + 1])
-{
-swap(&array[y], &array[y + 1]);
-print_array(array, size);
-swapped = 1;
-}
-}
-if (!swapped)
-break;
-}
+	size_t x, y;
+	int swapped;
+
+	for (x = 0; x < size - 1; x++)
+	{
+		swapped = 0;
+		for (y = 0; y < size - x - 1; y++)
+		{
+			if (array[y] > array[y + 1])
+			{
+				swap(&array[y], &array[y + 1]);
+				print_array(array, size);
+				swapped = 1;
+			}
+		}
+		if (!swapped)
+			break;
+	}
 }
